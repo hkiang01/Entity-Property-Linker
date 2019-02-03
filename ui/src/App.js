119aed1d579e1e1ab7008ca139d5844bc9f6934c
Harrison Kiang <hkiang01@gmail.com>
2019-02-03 04:19:24 -0500

    change title

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,6 +4,7 @@ import Linker from './linker/Linker.js';
 
 class App extends Component {
   render() {
+    document.title = "Entity-Property Linker"
     return (
       <div className="App">
         <div className="App-header">Entity-Property Linker</div>
